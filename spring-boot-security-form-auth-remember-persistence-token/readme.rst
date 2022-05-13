f012d2996792c707d837ceb77baddd3f169cc202
Geoff Vargo <geoff.vargo@stonybrook.edu>
2022-05-13 16:34:16 -0400

    Delete spring-boot-security-form-auth-remember-persistence-token directory

--- spring-boot-security-form-auth-remember-persistence-token/readme.rst
+++
@@ -1,1 +0,0 @@
-You can go through the tutorial https://www.roytuts.com/spring-boot-security-form-based-authentication-persistence-token-remember-me/
--- spring-boot-security-form-auth-remember-persistence-token/src/main/resources/persistent_logins.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `persistent_logins`;
-
-CREATE TABLE `persistent_logins` (
-  `username` varchar(64) NOT NULL,
-  `series` varchar(64) NOT NULL,
-  `token` varchar(64) NOT NULL,
-  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`series`)
-);
